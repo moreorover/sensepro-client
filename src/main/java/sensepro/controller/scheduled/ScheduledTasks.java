93263853ca4b353855c03a54d0453fc387ab162d
Martin <mselvenis@gmail.com>
2025-01-28 21:46:16 +0000

    fix: task to trigger buttons

--- src/main/java/sensepro/controller/scheduled/ScheduledTasks.java
+++ src/main/java/sensepro/controller/scheduled/ScheduledTasks.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import sensepro.controller.mq.MessagePublisher;
+import sensepro.controller.service.PinService;
+import sensepro.controller.service.PinServiceImpl;
 
 import java.time.LocalDateTime;
 
@@ -12,9 +14,11 @@ import java.time.LocalDateTime;
 public class ScheduledTasks {
 
     private final MessagePublisher messagePublisher;
+    private final PinServiceImpl pinService;
 
-    public ScheduledTasks(MessagePublisher messagePublisher) {
+    public ScheduledTasks(MessagePublisher messagePublisher, PinServiceImpl pinService) {
         this.messagePublisher = messagePublisher;
+        this.pinService = pinService;
     }
 
 //    @Scheduled(cron = "*/30 * * * * *")
@@ -23,5 +27,21 @@ public class ScheduledTasks {
         // Task logic goes here
         log.info("Task 1 executed at {}", now);
         messagePublisher.sendMessage("system", "message: " + now);
+    }
+
+    @Scheduled(cron = "*/30 * * * * *")
+    public void task2() {
+        LocalDateTime now = LocalDateTime.now();
+        // Task logic goes here
+        log.info("Task 2 executed at {}", now);
+        log.info("Total buttons: {}", pinService.getPinMap().size());
+        pinService.getPinMap().forEach((key, value) -> {
+            log.info("Button key: {}", key);
+            log.info("Button name: {}", value.getName());
+            log.info("Setting high...");
+            value.isHigh();
+            log.info("Setting low...");
+            value.isLow();
+        });
     }
 }
